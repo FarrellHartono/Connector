348e6e9959dc115a7a0cee347fff4e6add7547e5
farrellhartono <farrellhartono@gmail.com>
2024-08-15 07:07:26 +0000

    push laravel tailwind

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(){
+        return view('login');
+    }
+
+    public function loginProcess(Request $request){
+        $request->validate([
+            "email" => "required|email",
+            "password"=>"required"
+        ]);
+
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+
+        if(Auth::attempt($data)){
+            return redirect(route('home'));
+        }else{
+            return redirect(route('register'));
+        }
+    }  
+}
