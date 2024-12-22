d488136b3e95ec86f1a0e56c83f782a1504dbb30
christoferc <christofer.cuthbert@gmail.com>
2024-12-22 19:41:31 +0700

    navbar changes

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,6 +1,9 @@
 <div id="modal" class="fixed w-screen h-full bg-black opacity-50 z-50 hidden">
 </div>
-<nav class="bg-white dark:bg-gray-900 w-full z-20 top-0 border-b border-gray-200 dark:border-gray-600 flex justify-evenly">
+<nav class="bg-white dark:bg-gray-900 w-full px-1 z-20 top-0 border-b border-gray-200 dark:border-gray-600 flex justify-evenly">
+    <div class="text-lg font-bold self-center">
+        Connector<span class="text-blue-500">App</span>
+    </div>
     <div class="flex flex-wrap items-center justify-between p-4 w-full">
         <!-- <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
         <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo">
@@ -60,7 +63,7 @@
 
 
     <div class="items-center justify-center flex-grow hidden md:flex" id="navbar-sticky">
-        <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-12 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <ul class="hidden md:flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-12 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
                 <a href="{{ route('home') }}"
                    class="group relative block py-2 px-3 {{ request()->routeIs('home') ? 'text-blue-700 font-bold' : 'text-gray-900' }} rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
@@ -101,6 +104,54 @@
             </li>
             @endif
         </ul>
+        {{-- hamburger menu --}}
+        <button class="space-y-1 group md:hidden">
+            <div class="w-6 h-1 bg-black"></div>
+            <div class="w-6 h-1 bg-black"></div>
+            <div class="w-6 h-1 bg-black"></div>
+
+            <ul class="bg-[#252525] w-screen pb-10 absolute -top-0 right-0 duration-150 flex flex-col space-y-3 justify-end">
+                <li>
+                    <a href="{{ route('home') }}"
+                       class=""
+                       aria-current="page">
+                        Home
+                        <span class="absolute bottom-0 left-1/2 w-0 h-0.5 bg-blue-700 transform -translate-x-1/2 transition-all duration-300 group-hover:w-full"></span>
+                    </a>
+                </li>
+                @if (auth()->id() !== 1)
+                <li>
+                    <a href="{{ route('listBusiness') }}"
+                       class="">
+                        My Business
+                        <span class="absolute bottom-0 left-1/2 w-0 h-0.5 bg-blue-700 transform -translate-x-1/2 transition-all duration-300 group-hover:w-full"></span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('profile') }}"
+                       class="">
+                        Profile
+                        <span class="absolute bottom-0 left-1/2 w-0 h-0.5 bg-blue-700 transform -translate-x-1/2 transition-all duration-300 group-hover:w-full"></span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#"
+                       class="group relative block py-2 px-3 {{ request()->routeIs('approval') ? 'text-blue-700 font-bold' : 'text-gray-900' }} rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                        Approval
+                        <span class="absolute bottom-0 left-1/2 w-0 h-0.5 bg-blue-700 transform -translate-x-1/2 transition-all duration-300 group-hover:w-full"></span>
+                    </a>
+                </li>
+                @else
+                <li>
+                    <a href="{{ route('admin.businesses') }}"
+                       class="group relative block py-2 px-3 {{ request()->routeIs('admin.businesses') ? 'text-blue-700 font-bold' : 'text-gray-900' }} rounded hover:bg-gray-100 md:hover:bg-transparent md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                        Business Approval
+                        <span class="absolute bottom-0 left-1/2 w-0 h-0.5 bg-blue-700 transform -translate-x-1/2 transition-all duration-300 group-hover:w-full"></span>
+                    </a>
+                </li>
+                @endif
+            </ul>
+        </button>
       </div>
     </div>
 
