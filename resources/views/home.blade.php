1f22cfb7bb24f4c3b0356b65407f2f5e17330dd1
FarrellHartono <farrellhartono@gmail.com>
2025-01-06 22:08:49 +0700

    -Benerin UI di home, businessDetail, listBusiness

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -27,7 +27,7 @@
 </div>
 
 
-<div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4  gap-3">
+<div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-3">
     @foreach($businesses as $business)
     @php
         $folderPath = $business->image_path;
@@ -42,8 +42,8 @@
         }
     }
     @endphp
-        <div class="flex-1 w-full max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <img src="{{ asset('storage/' . str_replace('public/', '', $filePath)) }}" />
+        <div class="flex-1 max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <img class= "w-full h-64 rounded-lg" src="{{ asset('storage/' . str_replace('public/', '', $filePath)) }}" />
 
             <a href="{{ route('business.show', $business->id) }}">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $business->title }}</h5>
