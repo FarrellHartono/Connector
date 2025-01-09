2dc112b9c25ba01bdb11bb4dc76b7a5028e8fe38
rickyzaini <zainiricky@gmail.com>
2025-01-09 23:43:29 +0700

    tampilan show more list bisnis

--- resources/views/listBusiness.blade.php
+++ resources/views/listBusiness.blade.php
@@ -41,7 +41,7 @@
                         <div class="flex">
                             <img src="{{ asset('storage/' . str_replace('public/', '', $filePath)) }}"
                                 class="w-64 max-h-full object-cover rounded mr-4" />
-                            <div>
+                            <div class="w-2/3">
                                 <a href="{{ route('manageBusiness', $business->id) }}">
                                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                                         {{ $business->title }}</h5>
@@ -51,10 +51,7 @@
                                 <p class="flex mb-3 font-normal text-gray-700 dark:text-gray-400">
                                     {{ \Illuminate\Support\Str::limit($business->description, 100, '...') }}
 
-                                    <a href="{{ route('business.show', $business->id) }}"
-                                        class="ml-10 inline-flex items-center px-3 py-3 text-sm font-medium text-center text-white bg-blue-700 rounded-full hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                        <x-svg-icon name="right-arrow" />
-                                    </a>
+                                    
                                 </p>
                                 <a href="{{ route('manageBusiness', $business->id) }}"
                                     class="inline-flex items-center mt-7 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
@@ -73,6 +70,12 @@
                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                             stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                                     </svg>
+                                </a>
+                            </div>
+                            <div class="w-1/3 self-center">
+                                <a href="{{ route('business.show', $business->id) }}"
+                                    class="ml-10 inline-flex items-center px-3 py-3 text-sm font-medium text-center text-white bg-blue-700 rounded-full hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                    <x-svg-icon name="right-arrow" />
                                 </a>
                             </div>
                         </div>
